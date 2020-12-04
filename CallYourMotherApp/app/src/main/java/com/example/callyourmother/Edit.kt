f2861a4c99d8badb2d9bb51b6e67ecaa4398599d
ktp312 <ktp312@gmail.com>
2020-12-04 04:05:48 -0500

    all together now

---
+++ CallYourMotherApp/app/src/main/java/com/example/callyourmother/Edit.kt
@@ -0,0 +1,98 @@
+package com.example.callyourmother
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+
+
+class Edit : AppCompatActivity() {
+    private lateinit var name: TextView
+    private lateinit var numTimes: EditText
+    private  var reminderType: String? = null
+    private var str:String? = null
+    private lateinit var saveButton: Button
+    private lateinit var selectedRadioButton: RadioGroup
+    private lateinit var dayRadio: RadioButton
+    private lateinit var weekRadio: RadioButton
+    private lateinit var monthRadio: RadioButton
+    private lateinit var yearRadio: RadioButton
+    private lateinit var textChecked: CheckBox
+    private lateinit var callChecked: CheckBox
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+
+        name = findViewById(R.id.contactName)
+        numTimes = findViewById(R.id.editTextNumber)
+        saveButton = findViewById(R.id.saveFreq)
+        dayRadio = findViewById(R.id.dayFreq)
+        weekRadio = findViewById(R.id.weekFreq)
+        monthRadio = findViewById(R.id.monthFreq)
+        yearRadio = findViewById(R.id.yearFreq)
+        callChecked = findViewById(R.id.CallcheckBox)
+        textChecked = findViewById(R.id.textCheckBox)
+
+        // TODO: get the contact's name from mainActivity
+        // probably through an intent
+
+
+        // Are we editing calls, texts or boths
+        if(callChecked.isChecked && textChecked.isChecked)
+            reminderType = "both"
+        else if(callChecked.isChecked)
+            reminderType = "call"
+        else if(textChecked.isChecked)
+            reminderType = "text"
+
+
+        // Saving user configurations
+        saveButton.setOnClickListener {
+
+            // TODO - gather ToDoItem data
+            Toast.makeText(applicationContext, reminderType+" reminders set "+ numTimes.text.toString()+" times "+str, Toast.LENGTH_LONG).show()
+
+
+            // TODO - return data Intent to main activity where this will also be sent to notification data
+
+            /*   var dataIntent: Intent = Intent(this, SecondActivity.class)
+               dataIntent.putExtra("reminder type", reminderType)
+               dataIntent.putExtra("number of times", numTimes.toString())
+               dataIntent.putExtra("frequency type", str)
+               startActivity(dataIntent) */
+
+        }
+
+
+    }
+
+
+    fun onCheckBoxClicked(view: View) {
+        if(callChecked.isChecked && textChecked.isChecked)
+            reminderType = "both"
+        else if(callChecked.isChecked)
+            reminderType = "call"
+        else if(textChecked.isChecked)
+            reminderType = "text"
+        //Toast.makeText(applicationContext, reminderType+" reminder types selected", Toast.LENGTH_SHORT).show()
+    }
+
+    // Get how often to be reminded such as by days, weeks, months or years
+    fun onRadioButtonClicked(view: View) {
+        if (dayRadio.isChecked)
+            str = "Daily"
+        else if(weekRadio.isChecked)
+            str = "Weekly"
+        else if (monthRadio.isChecked)
+            str = "Monthly"
+        else
+            str = "Yearly"
+
+        Toast.makeText(applicationContext, str+" reminder selected", Toast.LENGTH_SHORT).show()
+    }
+}
--- CallYourMotherApp/app/src/main/java/com/example/callyourmother/MainActivity.kt
+++ CallYourMotherApp/app/src/main/java/com/example/callyourmother/MainActivity.kt
@@ -1,90 +1,153 @@
 package com.example.callyourmother
 
 import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ListView
 import android.widget.SearchView
 
 import android.widget.SimpleCursorAdapter
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    //test!!!!
+    var cols = listOf<String>(
+        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+        ContactsContract.CommonDataKinds.Phone.NUMBER,
+        ContactsContract.CommonDataKinds.Phone._ID //bind the data
+    ).toTypedArray()
 
-        var cols = listOf<String>(
-                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                ContactsContract.CommonDataKinds.Phone.NUMBER,
-                ContactsContract.CommonDataKinds.Phone._ID //bind the data
-        ).toTypedArray()
+    private val CHANNEL_ID = "channel_id"
+    public var notificationId = 101
+    private val time = 0 //in milliseconds
+    private val mNotificationTime = Calendar.getInstance().timeInMillis + time
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
-            setSupportActionBar(findViewById(R.id.toolbar))
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
-            findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-            }
-
-            //ask for permission multiple times
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, Array(1) { Manifest.permission.READ_CONTACTS }, 111)
-            } else {
-                readContact()
-            }
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
 
-        override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-            if(requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                readContact()
-            }
+
+        createNotificationChannel()
+
+        button2.setOnClickListener {
+            sendNotification()
         }
-
-        private fun readContact(){
-            var from = listOf<String>( ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                    ContactsContract.CommonDataKinds.Phone.NUMBER).toTypedArray()
-
-            var to = intArrayOf(android.R.id.text1, android.R.id.text2)
-
-            //sort based on name
-            var rs = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                    cols, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
-
-
-
-            var adapter = SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, rs, from, to, 0)
-
-            listView.adapter = adapter
-
-            searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
-                override fun onQueryTextSubmit(p0: String?): Boolean {
-                    return false
-                }
-
-                override fun onQueryTextChange(p0: String?): Boolean {
-                    var rs = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                            cols,
-                            "${ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME} LIKE ?",
-                            Array(1){"%$p0%"},
-                            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
-                    adapter.changeCursor(rs)
-                    return false
-                }
-
-//            override fun onQueryTextSubmit(query: String?): Boolean {
-//                Log.i(TAG,"Llego al querysubmit")
-//                return false
+//            findViewById<ListView>(R.id.listView).setOnClickListener { parent, view, position, id ->
+//                // Get the selected item text from ListView
+//                val selectedItem = parent.getItemAtPosition(position) as String
+//                Log.i("tag", "gh")
+//                Toast.makeText(this, "The best player is $selectedItem", Toast.LENGTH_LONG).show()//
+//
 //            }
-            })
+//            val listView : ListView = findViewById(R.id.listView)
+
+//            listView.setOnItemClickListener { parent, view, position, id ->
+//                // Get the selected item text from ListView
+//                val selectedItem = parent.getItemAtPosition(position) as String
+//                Log.i("tag", "gh")
+//                Toast.makeText(this, "The best player is $selectedItem", Toast.LENGTH_LONG).show()//
+//
+//            }
+
+//            listView.setOnItemClickListener { parent, _, position, _ ->
+//                val selectedItem = parent.getItemAtPosition(position) as String
+////                Toast.makeText(this, "The best player is $selectedItem", Toast.LENGTH_LONG).show()//
+//                    Log.i("tsg","msg")
+//            }
+
+
+
+        //ask for permission multiple times
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, Array(1) { Manifest.permission.READ_CONTACTS }, 111)
+        } else {
+            readContact()
         }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            readContact()
+        }
+    }
+
+    private fun readContact(){
+        var from = listOf<String>( ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+            ContactsContract.CommonDataKinds.Phone.NUMBER).toTypedArray()
+
+//            Log.i("tag", "ahh")
+
+        var to = intArrayOf(android.R.id.text1, android.R.id.text2)
+
+        //sort based on name
+        var rs = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            cols, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+
+
+//val adapter = ArrayAdapter(this, R.layout.book_list_item, R.id.book_title, array)
+        var adapter = SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, rs, from, to, 0)
+//            val listView : ListView = findViewById(R.id.listView)
+        listView.adapter = adapter
+
+
+//            listView.setOnItemClickListener { parent, view, position, id ->
+//                val element = adapter.getItemAtPosition(position) // The item that was clicked
+//            }
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val element = adapter.getItem(position)// The item that was clicked
+//                val intent = Intent(this, BookDetailActivity::class.java)
+//                startActivity(intent)
+            Log.i("tag", "onclickcontactyay")
+
+            Toast.makeText(this, "The best player is $element", Toast.LENGTH_SHORT).show()//
+
+                val intent = Intent(this, Edit::class.java)
+                startActivity(intent)
+        }
+
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                var rs = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                    cols,
+                    "${ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME} LIKE ?",
+                    Array(1){"%$p0%"},
+                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+                adapter.changeCursor(rs)
+                return false
+            }
+
+        })
+    }
 
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -102,6 +165,71 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_HIGH//
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotification(){
+
+        val landingIntent = Intent(this, Receiver::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, landingIntent, 0)
+
+        val callnowIntent = Intent(this, Receiver::class.java).apply {
+            identifier = "now"
+//            action = Context.ALARM_SERVICE
+        }
+//        callnowIntent.identifier = "now"
+        val callnowPendingIntent: PendingIntent = PendingIntent.getBroadcast(this, 0, callnowIntent, 0)
+
+        val snoozeIntent = Intent(this, Receiver::class.java).apply {
+            identifier = "later"
+        }
+        val snoozePendingIntent: PendingIntent = PendingIntent.getBroadcast(this, 0, snoozeIntent, 0)
+
+        val tomorrowIntent = Intent(this, Receiver::class.java).apply {
+            identifier = "tomorrow"
+        }
+        val tomorrowPendingIntent: PendingIntent = PendingIntent.getBroadcast(this, 0, tomorrowIntent, 0)
+
+
+
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("AHHHHHHHHHHHHHHHHHHHHHH")
+            .setContentText("help me :(")
+//            .setAutoCancel(true)
+//            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+            .addAction(R.drawable.ic_launcher_foreground, "Call Now", callnowPendingIntent)
+            .addAction(R.drawable.ic_launcher_foreground, "Snooze for 2 hours", snoozePendingIntent)
+            .addAction(R.drawable.ic_launcher_foreground, "Call Tomorrow", tomorrowPendingIntent)
+            .setAutoCancel(true)
+
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId, notificationBuilder.build())
+        }
+
+    }
+
+
 
 
 }
